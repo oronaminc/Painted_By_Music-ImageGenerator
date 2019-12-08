979717e2ec1347de0a3620990bcdcfbce33f3808
sunghyeon <sunghyeon86@gmail.com>
2019-12-08 21:11:57 +0900

    find bug

--- run.py
+++ run.py
@@ -21,5 +21,5 @@ print("=============Find Urls Done================")
 style = "https://i.pinimg.com/originals/82/0f/c3/820fc362b0e1fdf18f755e498bd34f98.jpg"
 Url = get_image(Urls[2], style)
 N = len(glob.glob(Path))
-urllib.request.urlretrieve(Url['output_url'], Path+"output"+str(N)+".png")
+urllib.request.urlretrieve(Url['output_url'], Path+"output"+str(N+1)+".png")
 print("=============Create Image Done================")
