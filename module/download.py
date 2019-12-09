cad16b9d436ed4bc263d304e1bf96a22b494a85c
sunghyeon <sunghyeon86@gmail.com>
2019-12-10 00:52:34 +0900

    Add Django Server

--- module/download.py
+++ module/download.py
@@ -1,13 +1,16 @@
 from google_images_download import google_images_download 
 import sys
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
-def get_urls(keyword, limit):
+def get_urls(keyword, limit, isContent):
     orig_stdout = sys.stdout
     f = open('URLS.txt', 'w')
     sys.stdout = f
 
     response = google_images_download.googleimagesdownload()
 
+    if not isContent: keyword += ' painting'
     arguments = {"keywords"     : keyword,
                 "limit"        : limit,
                 "print_urls"   : True,
@@ -22,10 +25,20 @@ def get_urls(keyword, limit):
         content = f.readlines()
     f.close()
 
+    # url 리스트에 삽입, 단 url 검사하여 http 에러 있는 것은 집어넣지 않음
     urls = []
     for j in range(len(content)):
         if content[j][:9] == 'Completed':
-            urls.append(content[j-1][11:-1])   
+            url = content[j-1][11:-1]
+
+            try:
+                res = urlopen(url)
+            except HTTPError:
+                continue
+
+            urls.append(url)
+
+    urls = urls[:3] 
     
     return urls
     #for item in urls:
